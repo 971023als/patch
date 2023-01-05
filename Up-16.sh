5376b5c72a744c3989204b6e7ea74a1a46ac0294
971023als <971023als@naver.com>
2023-01-05 15:59:43 +0900

    dsfa

--- Up-16.sh
+++ Up-16.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
-cd ..
-cd ..
-cd ..
-cd ..
 cd /tmp
 wget https://sourceforge.net/projects/rkhunter/files/rkhunter/1.4.6/rkhunter-1.4.6.tar.gz
 tar zxvf rkhunter-1.4.6.tar.gz
 cd rkhunter-1.4.6
-./installer.sh --layout /usr/local --install
+sudo ./installer.sh --layout custom /usr/local --install
 PATH=$PATH:/usr/local/bin
-/usr/local/bin/rkhunter --propupd
-/usr/local/bin/rkhunter --update
+sudo /usr/local/bin/rkhunter --propupd
+sudo /usr/local/bin/rkhunter --update
