e2655aa692be29e10a40bd82d14bf803d743fa49
971023als <971023als@naver.com>
2023-01-06 13:09:13 +0900

    sd

--- Up-16.sh
+++ Up-16.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 cd /tmp
 wget https://sourceforge.net/projects/rkhunter/files/rkhunter/1.4.6/rkhunter-1.4.6.tar.gz
 tar zxvf rkhunter-1.4.6.tar.gz
@@ -7,3 +8,54 @@ sudo ./installer.sh --layout custom /usr/local --install
 PATH=$PATH:/usr/local/bin
 sudo /usr/local/bin/rkhunter --propupd
 sudo /usr/local/bin/rkhunter --update
+
+
+. function.sh
+
+ 
+
+TMP1=./log/`SCRIPTNAME`.log
+
+> $TMP1
+
+ 
+
+BAR
+
+CODE [U-16] /dev에 존재하지 않는 device 파일 점검
+
+cat << EOF >> $RESULT
+
+[양호]: dev에 대한 파일 점검 후 존재하지 않은 device 파일을 제거한 경우
+
+[취약]: dev에 대한 파일 미점검, 또는, 존재하지 않은 device 파일을 방치한 경우
+
+EOF
+
+BAR
+
+ 
+
+ 
+
+find /dev -type f -exec ls -l {} \; > $TMP1
+
+ 
+
+if [ -s $TMP1 ] ; then
+
+WARN 파일이 존재합니다. 
+
+INFO $TMP1를 확인하십시오.
+
+else
+
+OK 파일이 존재하지 않습니다. 
+
+fi
+
+ 
+
+echo >>$RESULT
+
+echo >>$RESULT
---
+++ vuln_check_script.sh
@@ -0,0 +1,135 @@
+#!/bin/bash
+
+echo "-----------check list-----------"
+
+sh​ ./UP-01.​sh
+
+​​sh ./UP-02.​sh
+
+​​​sh ./UP-03.​​sh
+
+​​​sh ./UP-04.​​sh
+
+​​​sh ./UP-05.​​sh
+
+​​​sh ./UP-06.​​sh
+
+​​​sh ./UP-07.​​sh
+
+​​​sh ./UP-08.​​sh
+
+​​​sh ./UP-09.​​sh
+
+​​​sh ./UP-10.​​sh
+
+​​​sh ./UP-11.​​sh
+
+​​​sh ./UP-12.​​sh
+
+​​​sh ./UP-13.​​sh
+
+​​​sh ./UP-14.​​sh
+
+​​​sh ./UP-15.​​sh
+
+​​​sh ./UP-16.​​sh
+
+​​​sh ./UP-17.​​sh
+
+​​​sh ./UP-18.​​sh
+
+​​​sh ./UP-19.​​sh
+
+​​​sh ./UP-20.​​sh
+
+​​​sh ./UP-21.​​sh
+
+​​​sh ./UP-22.​​sh
+
+​​​sh ./UP-24.​​sh
+
+​​​sh ./UP-28.​​sh
+
+​​​sh ./UP-30.​​sh
+
+​​​sh ./UP-32.​​sh
+
+​​​sh ./UP-34.​​sh
+
+​​​sh ./UP-35.​​sh
+
+​​​sh ./UP-36.​​sh
+
+​​​sh ./UP-37.​​sh
+
+​​​sh ./UP-38.​​sh
+
+​​​sh ./UP-39.​​sh
+
+​​​sh ./UP-40.​​sh
+
+​​​sh ./UP-41.​​sh
+
+​​​sh ./UP-42.​​sh
+
+​​​sh ./UP-43.​​sh
+
+​​​sh ./UP-44.​​sh
+
+​​​sh ./UP-45.​​sh
+
+​​​sh ./UP-46.​​sh
+
+​​​sh ./UP-47.​​sh
+
+​​​sh ./UP-48.​​sh
+
+​​​sh ./UP-49.​​sh
+
+​​​sh ./UP-50.​​sh
+
+​​​sh ./UP-51.​​sh
+
+​​​sh ./UP-52.​​sh
+
+​​​sh ./UP-53.​​sh
+
+​​​sh ./UP-54.​​sh
+
+​​​sh ./UP-55.​​sh
+
+​​sh ./UP-56.​​sh
+
+​​sh ./UP-57.​​sh
+
+​​sh ./UP-58.​​sh
+
+​​sh ./UP-59.​​sh
+
+​​sh ./UP-60.​​sh
+
+​​sh ./UP-61.​​sh
+
+​​sh ./UP-62.​​sh
+
+​​sh ./UP-63.​​sh
+
+​​sh ./UP-64.​​sh
+
+​​sh ./UP-65.​​sh
+
+​​sh ./UP-66.​​sh
+
+​​sh ./UP-67.​​sh
+
+​​sh ./UP-68.​​sh
+
+​​sh ./UP-69.​​sh
+
+​​sh ./UP-70.​​sh
+
+​​sh ./UP-71.​​sh
+
+​​sh ./UP-72.​​sh
+
+​​sh ./UP-72.​​sh
